5f06ac36ec108be10e19c3990619f0f733253d6b
Gilbert Tan <gilbert.tan1991@gmail.com>
2024-06-09 03:41:52 +0800

    Add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'raw-umber': 'hsla(23, 28%, 40%, 1)',
-        sienna: 'hsla(9, 83%, 29%, 1)',
-        'persian-orange': 'hsla(26, 51%, 59%, 1)',
-        'tigers-eye': 'hsla(30, 70%, 40%, 1)',
-        'hunyadi-yellow': 'hsla(39, 74%, 68%, 1)',
+        "rosso-corsa": "#D50A07",
+        "olive-drab3": "#729E11",
+        "maize-crayola": "#F2C246",
+        "desert-sand": "#ECC6A8",
+        onyx: "#363937",
       },
     },
   },
   plugins: [],
-}
+};
